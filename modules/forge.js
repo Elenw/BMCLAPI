469b12162827a3d93c03d04ad33d9585b6458f1a
bangbang93 <bangbang93@163.com>
2013-12-29 08:00:41 +0800

    支持1.6和1.7

--- modules/forge.js
+++ modules/forge.js
@@ -2,6 +2,7 @@ var request = require('request');
 var $ = require('cheerio').load();
 
 exports.getForgeList = function(callback) {
+    var storage = [];
     request('http://files.minecraftforge.net', {
         method: 'GET'
     }, function(err, res, body) {
@@ -10,7 +11,6 @@ exports.getForgeList = function(callback) {
         } else {
             var htmlDom = $(body);
             var promo = htmlDom.find('#promotions_table');
-            var storage = [];
             promo.find('tr').each(function(index) {
                 if (index > 0) {
                     console.log(index);
@@ -49,11 +49,56 @@ exports.getForgeList = function(callback) {
                                             forgever.universal = $(this).attr('href');
                                     }
                                 });
+                                break;
                         }
                     });
+                    storage.push(forgever);
                 }
-                storage.push(forgever);
-                console.log(forgever);
+
+                var build = htmlDom.find('.builds');
+                build.each(function(index) {
+                    var table = $(this).find('table');
+                    table.find('tr').each(function(index) {
+                        if (index > 0) {
+                            var forgever = {};
+                            $(this).find('td').each(function(index) {
+                                switch (index) {
+                                    case 0:
+                                        forgever.ver = $(this).text();
+                                        break;
+                                    case 1:
+                                        forgever.mcver = $(this).text();
+                                        break;
+                                    case 2:
+                                        forgever.releasetime = $(this).text();
+                                        break;
+                                    case 3:
+                                        $(this).find('a').each(function(index) {
+                                            var text = $(this).text();
+                                            switch (text) {
+                                                case 'Changelog':
+                                                    forgever.changelog = $(this).attr('href');
+                                                    break;
+                                                case 'Installer':
+                                                    forgever.installer = $(this).attr('href');
+                                                    break;
+                                                case 'Javadoc':
+                                                    forgever.javadoc = $(this).attr('href');
+                                                    break;
+                                                case 'Src':
+                                                    forgever.src = $(this).attr('href');
+                                                    break;
+                                                case 'Universal':
+                                                    forgever.universal = $(this).attr('href');
+                                            }
+                                        });
+                                        break;
+                                }
+                            });
+                            storage.push(forgever);
+                        }
+                    });
+                });
             });
             callback(null, storage);
         }
